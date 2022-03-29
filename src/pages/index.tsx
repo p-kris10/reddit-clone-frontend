5ed407e9e13190abf8cd480a3a3382dd9aba5fc8
Deadmau26 <26pkristen@gmail.com>
2022-03-29 16:57:56 +0530

    upvote downvote logic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import { Layout } from "../components/Layout";
 import { Box, Button, Flex, Heading, Link, Stack,Text} from "@chakra-ui/react";
 import NextLink from "next/link"
 import { useState } from "react";
+import { UpdootSection } from "../components/UpdootSection";
 
 const Index = () => {
   const [variables,setVariables] = useState({limit:15,cursor:null as null | string});
@@ -18,40 +19,48 @@ const Index = () => {
   {
     return <div>you got no posts for some reason</div>
   }
-  return (
-    <Layout>   
+  return  (
+    <Layout>
       <Flex align="center">
         <Heading>LiReddit</Heading>
         <NextLink href="/create-post">
-        <Link ml="auto">create a post</Link>
+          <Link ml="auto">create post</Link>
         </NextLink>
       </Flex>
-      
-     
       <br />
       {!data && fetching ? (
         <div>loading...</div>
       ) : (
         <Stack spacing={8}>
-        {data!.posts.posts.map((p) => 
-        <Box key={p.id} p={5} shadow='md' borderWidth='1px' >
-        <Heading fontSize='xl'>{p.title}</Heading>
-        <Text>posted by {p.creator.username}</Text>
-        <Text mt={4}>{p.textSnippet}</Text>
-        </Box>)}
-        
-
+          {data!.posts.posts.map((p) => (
+            <Flex key={p.id} p={5} shadow="md" borderWidth="1px">
+              <UpdootSection post={p} />
+              <Box>
+                <Heading fontSize="xl">{p.title}</Heading>
+                <Text>posted by {p.creator.username}</Text>
+                <Text mt={4}>{p.textSnippet}</Text>
+              </Box>
+            </Flex>
+          ))}
         </Stack>
       )}
-      {data && data.posts.hasMore?(
+      {data && data.posts.hasMore ? (
         <Flex>
-          <Button onClick={()=>{setVariables({limit: variables.limit,
-          cursor : data.posts.posts[data.posts.posts.length - 1].createdAt,}
-          )}}
-          isLoading={fetching} backgroundColor="tomato" m="auto" my={8} >Load More</Button>
+          <Button
+            onClick={() => {
+              setVariables({
+                limit: variables.limit,
+                cursor: data.posts.posts[data.posts.posts.length - 1].createdAt,
+              });
+            }}
+            isLoading={fetching}
+            m="auto"
+            my={8}
+          >
+            load more
+          </Button>
         </Flex>
-      ):null
-      }
+      ) : null}
     </Layout>
   );
 };
