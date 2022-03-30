596f1e920719eb667a96f63b8007a0e12048a265
Deadmau26 <26pkristen@gmail.com>
2022-03-30 15:17:22 +0530

    added delete post functionality

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Flex,Link } from "@chakra-ui/react";
+import { Box, Button, Flex,Heading,Link } from "@chakra-ui/react";
 import NextLink from 'next/link';
 import React from "react";
 import { useLogoutMutation, useMeQuery } from "../generated/graphql";
@@ -8,47 +8,61 @@ interface NavBarProps{
 
 }
 
-export const NavBar : React.FC<NavBarProps> = ({}) =>{
-    const[{data,fetching}] = useMeQuery({
-        pause : isServer(),
-    })
-    const [{fetching : LogoutFetching},logout] = useLogoutMutation()
-    let body = null
-    //data is loading
-    if(fetching)
-    {
-
+export const NavBar: React.FC<NavBarProps> = ({}) => {
+    const [{ fetching: logoutFetching }, logout] = useLogoutMutation();
+    const [{ data, fetching }] = useMeQuery({
+      pause: isServer(),
+    });
+  
+    let body = null;
+  
+    // data is loading
+    if (fetching) {
+      // user not logged in
+    } else if (!data?.me) {
+      body = (
+        <>
+          <NextLink href="/login">
+            <Link mr={2}>login</Link>
+          </NextLink>
+          <NextLink href="/register">
+            <Link>register</Link>
+          </NextLink>
+        </>
+      );
+      // user is logged in
+    } else {
+      body = (
+        <Flex align="center">
+          <NextLink href="/create-post">
+            <Button variant="solid" backgroundColor="white" mr={4}>
+              create post
+            </Button>
+          </NextLink>
+          <Box mr={2}>{data.me.username}</Box>
+          <Button
+            onClick={() => {
+              logout();
+            }}
+            isLoading={logoutFetching}
+            variant="link"
+          >
+            logout
+          </Button>
+        </Flex>
+      );
     }
-    //not logged in
-    else if(!data?.me)
-    {
-        body = (
-            <>
-            <NextLink href={"/login"}>
-                    <Link  mr={2}>Login </Link>
-                    </NextLink>
-                    <NextLink href={"/register"}>
-                    <Link>Register </Link>
-            </NextLink>
-            </>
-        )
-    }
-    else
-    {
-        body = (
-            <Flex>
-                <Box mr={2}>{data.me.username}</Box>
-                <Button onClick={()=>{logout();}} isLoading={LogoutFetching} variant="link">logout</Button>
-            </Flex>
-            
-        )
-    }
+  
     return (
-        <Flex zIndex={1} position="sticky" top={0} bg='tomato'>
-                <Box  p={4} ml={'auto'} >
-                    {body}
-                </Box>
-            </Flex>
-            
+      <Flex zIndex={1} position="sticky" top={0} bg="tan" p={4}>
+        <Flex flex={1} m="auto" align="center" maxW={800}>
+          <NextLink href="/">
+            <Link>
+              <Heading>LiReddit</Heading>
+            </Link>
+          </NextLink>
+          <Box ml={"auto"}>{body}</Box>
+        </Flex>
+      </Flex>
     );
-}
+  };
